525a39ec1aa41f60816b13387a3c29e85b7fea90
Artem Balashov <pockerhead@ya.ru>
2021-08-30 15:00:06 +0300

    added mainOrAsync, added Engine file

---
+++ FibKit/AsyncLayout/Engine.swift
@@ -0,0 +1,13 @@
+//
+//  Engine.swift
+//  FibKit
+//
+//  Created by Артём Балашов on 30.08.2021.
+//  Copyright © 2021 DIT Moscow. All rights reserved.
+//
+
+import UIKit
+
+protocol LayoutElement {
+    var frame: CGRect  { get set }
+}
--- FibKit/Utils/CommonUtils.swift
+++ FibKit/Utils/CommonUtils.swift
@@ -8,6 +8,16 @@
 
 import Foundation
 
+func mainOrAsync(_ closure: @escaping (() -> Void)) {
+    if Thread.isMainThread {
+        closure()
+    } else {
+        DispatchQueue.main.async {
+            closure()
+        }
+    }
+}
+
 func delay(_ delay: Double, closure:@escaping () -> Void) {
     DispatchQueue.main.asyncAfter(
         deadline: DispatchTime.now() + Double(Int64(delay * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: closure)
