d13f4723de408cb0bb96a4f9058c137089cf968c
Artem Balashov <pockerhead@ya.ru>
2023-07-18 09:06:03 +0300

    added example app in git

---
+++ FibExampleApp/FibExampleApp/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  FibExampleApp
+//
+//  Created by Артём Балашов on 18.07.2023.
+//
+
+import FibKit
+
+class ViewController: FibViewController {
+
+	@SectionBuilder
+	override var sections: [GridSection] {
+		GridSection {
+			FormViewSpacer(60)
+			MyFibView.ViewModel(text: "v53g42g42")
+		}
+	}
+
+}
+
+class MyFibView: UIView, ViewModelConfigurable {
+	
+	var label: UILabel = .init()
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		configureUI()
+	}
+	
+	required init?(coder: NSCoder) {
+		super.init(coder: coder)
+		configureUI()
+	}
+	
+	func configureUI() {
+		addSubview(label)
+	}
+	
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		label.frame = bounds
+		label.textAlignment = .center
+	}
+	
+	func sizeWith(_ targetSize: CGSize, data: ViewModelWithViewClass?, horizontal: UILayoutPriority, vertical: UILayoutPriority) -> CGSize? {
+		guard let data = data as? ViewModel else { return .zero }
+		configure(with: data)
+		let size = label.sizeThatFits(targetSize)
+		return .init(width: targetSize.width, height: size.height)
+	}
+	
+	func configure(with data: FibKit.ViewModelWithViewClass?) {
+		guard let data = data as? ViewModel else { return }
+		label.text = data.text
+	}
+	
+
+	
+	
+	class ViewModel: ViewModelWithViewClass {
+		internal init(text: String) {
+			self.text = text
+		}
+		
+		
+		var text: String
+		
+		func viewClass() -> FibKit.ViewModelConfigurable.Type {
+			MyFibView.self
+		}
+		
+		
+	}
+}
