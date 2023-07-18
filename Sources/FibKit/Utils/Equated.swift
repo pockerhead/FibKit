9f2871ab9cb09cb8af551bf074a5165b0155875d
Artem Balashov <pockerhead@ya.ru>
2023-07-18 09:25:29 +0300

    make Equated internal

--- Sources/FibKit/Utils/Equated.swift
+++ Sources/FibKit/Utils/Equated.swift
@@ -1,89 +1,89 @@
 import Foundation
 
 @propertyWrapper
-public struct Equated<Value>: Equatable {
-  public init<T>(compare comparator: Comparator) where Value == T? {
+internal struct Equated<Value>: Equatable {
+  internal init<T>(compare comparator: Comparator) where Value == T? {
 	self.init(.none, compare: comparator)
   }
   
-  public init(_ wrappedValue: Value, compare comparator: Comparator) {
+  internal init(_ wrappedValue: Value, compare comparator: Comparator) {
 	self.init(wrappedValue: wrappedValue, compare: comparator)
   }
 
-  public init(wrappedValue: Value, compare comparator: Comparator) {
+  internal init(wrappedValue: Value, compare comparator: Comparator) {
 	self.wrappedValue = wrappedValue
 	self.comparator = comparator
   }
 
-  public var wrappedValue: Value
-  public var comparator: Comparator
+  internal var wrappedValue: Value
+  internal var comparator: Comparator
 
-  public static func == (lhs: Equated<Value>, rhs: Equated<Value>) -> Bool {
+  internal static func == (lhs: Equated<Value>, rhs: Equated<Value>) -> Bool {
 	lhs.comparator.compare(lhs.wrappedValue, rhs.wrappedValue)
 	  && rhs.comparator.compare(rhs.wrappedValue, lhs.wrappedValue)
   }
 }
 
 extension Equated where Value: Equatable {
-  public init<T>() where Value == T? {
+  internal init<T>() where Value == T? {
 	self.init(wrappedValue: .none)
   }
   
-  public init(wrappedValue: Value) {
+  internal init(wrappedValue: Value) {
 	self.init(wrappedValue: wrappedValue, compare: .custom(==))
   }
 }
 
 extension Equated: Error where Value: Error {
-  public init(_ wrappedValue: Value) {
+  internal init(_ wrappedValue: Value) {
 	self.init(wrappedValue: wrappedValue)
   }
   
-  public init(wrappedValue: Value) {
+  internal init(wrappedValue: Value) {
 	self.init(
 	  wrappedValue: wrappedValue,
 	  compare: .localizedDescription
 	)
   }
   
-  public var localizedDescription: String { wrappedValue.localizedDescription }
+  internal var localizedDescription: String { wrappedValue.localizedDescription }
 }
 
 extension Equated: Hashable where Value: Hashable {
-  public func hash(into hasher: inout Hasher) {
+  internal func hash(into hasher: inout Hasher) {
 	wrappedValue.hash(into: &hasher)
   }
 }
 
 @available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
 extension Equated: Identifiable where Value: Identifiable {
-  public var id: Value.ID { wrappedValue.id }
+  internal var id: Value.ID { wrappedValue.id }
 }
 
 extension Equated {
-  public struct Comparator {
-	public let compare: (Value, Value) -> Bool
+  internal struct Comparator {
+	internal let compare: (Value, Value) -> Bool
   }
 }
 
 extension Equated.Comparator {
-  public static func custom(_ compare: @escaping (Value, Value) -> Bool) -> Self {
+  internal static func custom(_ compare: @escaping (Value, Value) -> Bool) -> Self {
 	return .init(compare: compare)
   }
   
-  public static func property<Property: Equatable>(
+  internal static func property<Property: Equatable>(
 	_ scope: @escaping (Value) -> Property
   ) -> Self {
 	return .init { scope($0) == scope($1) }
   }
   
-  public static func wrappedProperty<Wrapped, Property: Equatable>(
+  internal static func wrappedProperty<Wrapped, Property: Equatable>(
 	_ scope: @escaping (Wrapped) -> Property
   ) -> Self where Value == Optional<Wrapped> {
 	return .init { $0.map(scope) == $1.map(scope) }
   }
   
-  public static var dump: Self {
+  internal static var dump: Self {
 	.init { lhs, rhs in
 	  var (lhsDump, rhsDump) = ("", "")
 	  Swift.dump(lhs, to: &lhsDump)
@@ -92,7 +92,7 @@ extension Equated.Comparator {
 	}
   }
   
-  public static var typedDump: Self {
+  internal static var typedDump: Self {
 	.init { lhs, rhs in
 	  var (lhsDump, rhsDump) = ("\(type(of: lhs))", "\(type(of: rhs))")
 	  Swift.dump(lhs, to: &lhsDump)
@@ -103,7 +103,7 @@ extension Equated.Comparator {
 }
   
 extension Equated.Comparator where Value: Error {
-  public static var localizedDescription: Self {
+  internal static var localizedDescription: Self {
 	.property(\.localizedDescription)
   }
 }
