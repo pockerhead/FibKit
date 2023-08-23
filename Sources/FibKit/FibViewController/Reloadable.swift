0fb89a1cac1a2917a7f68db3f9bfa0c0f11e3035
Denis Sadakov <SadakovDo@it.mos.ru>
2023-08-23 14:02:32 +0300

    ReloadableObject was added

--- Sources/FibKit/FibViewController/Reloadable.swift
+++ Sources/FibKit/FibViewController/Reloadable.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Combine
 
 protocol HaveControllerProp: AnyObject {
 	var controller: FibViewController? { get set }
@@ -27,5 +28,29 @@ public class Reloadable<Value>: HaveControllerProp {
 	
 	public init(wrappedValue stored: Value) {
 		self.stored = stored
+	}
+}
+
+@propertyWrapper
+public class ReloadableObject<Value: ObservableObject>: HaveControllerProp {
+	
+	weak var controller: FibViewController?
+	private var stored: Value
+	private var cancellable: Set<AnyCancellable> = []
+	
+	public var wrappedValue: Value {
+		get { stored }
+		set {
+			stored = newValue
+			guard let controller = controller else { return }
+			controller.reload()
+		}
+	}
+	
+	public init(wrappedValue stored: Value) {
+		self.stored = stored
+		stored.objectWillChange.sink { [weak self] _ in
+			self?.controller?.reload()
+		}.store(in: &cancellable)
 	}
 }
