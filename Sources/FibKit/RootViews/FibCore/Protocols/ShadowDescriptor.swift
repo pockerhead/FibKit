5d3188d6c47801b8566e5c1cd375a9d6fd1222b9
Egor Dadugin <daduginea@it.mos.ru>
2024-01-30 14:51:40 +0300

    made ShadowDescriptor public

--- Sources/FibKit/RootViews/FibCore/Protocols/ShadowDescriptor.swift
+++ Sources/FibKit/RootViews/FibCore/Protocols/ShadowDescriptor.swift
@@ -5,7 +5,7 @@
 //  Created by Денис Садаков on 23.08.2023.
 //
 
-struct ShadowDescriptor {
+public struct ShadowDescriptor {
 	init(style: UIUserInterfaceStyle = UIScreen.main.traitCollection.userInterfaceStyle, color: UIColor = withStyle(light: .black, dark: .clear), alpha: Float = 0.12, x: CGFloat = 0, y: CGFloat = 2, blur: CGFloat = 4, spread: CGFloat = 0, useShadowPath: Bool = false) {
 		self.style = style
 		self.color = color
