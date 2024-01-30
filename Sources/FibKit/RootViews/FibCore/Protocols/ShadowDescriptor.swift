46fe076226aa68653e90842e2edb5b9d8d64a325
Egor Dadugin <daduginea@it.mos.ru>
2024-01-30 15:43:32 +0300

    fix access troubles

--- Sources/FibKit/RootViews/FibCore/Protocols/ShadowDescriptor.swift
+++ Sources/FibKit/RootViews/FibCore/Protocols/ShadowDescriptor.swift
@@ -6,7 +6,7 @@
 //
 
 public struct ShadowDescriptor {
-	public init(style: UIUserInterfaceStyle = UIScreen.main.traitCollection.userInterfaceStyle, color: UIColor = withStyle(light: .black, dark: .clear), alpha: Float = 0.12, x: CGFloat = 0, y: CGFloat = 2, blur: CGFloat = 4, spread: CGFloat = 0, useShadowPath: Bool = false) {
+	init(style: UIUserInterfaceStyle = UIScreen.main.traitCollection.userInterfaceStyle, color: UIColor = withStyle(light: .black, dark: .clear), alpha: Float = 0.12, x: CGFloat = 0, y: CGFloat = 2, blur: CGFloat = 4, spread: CGFloat = 0, useShadowPath: Bool = false) {
 		self.style = style
 		self.color = color
 		self.alpha = alpha
@@ -16,7 +16,18 @@ public struct ShadowDescriptor {
 		self.spread = spread
 		self.useShadowPath = useShadowPath
 	}
-	
+
+	public init (color: UIColor = withStyle(light: .black, dark: .clear), alpha: Float = 0.12, x: CGFloat = 0, y: CGFloat = 2, blur: CGFloat = 4, spread: CGFloat = 0, useShadowPath: Bool = false) {
+		.init(style: UIScreen.main.traitCollection.userInterfaceStyle,
+			  color: color,
+			  alpha: alpha,
+			  x: x,
+			  y: y,
+			  blur: blur,
+			  spread: spread,
+			  useShadowPath: useShadowPath)
+	}
+
 	
 	static var `default` = ShadowDescriptor()
 	
