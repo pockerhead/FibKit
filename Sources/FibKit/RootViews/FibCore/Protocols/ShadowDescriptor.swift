c502ef200bed8e03137269cadd9951acf47d8f86
Egor Dadugin <daduginea@it.mos.ru>
2024-01-30 15:46:24 +0300

    fixed shadowDescriptor init

--- Sources/FibKit/RootViews/FibCore/Protocols/ShadowDescriptor.swift
+++ Sources/FibKit/RootViews/FibCore/Protocols/ShadowDescriptor.swift
@@ -17,15 +17,15 @@ public struct ShadowDescriptor {
 		self.useShadowPath = useShadowPath
 	}
 
-	public init (color: UIColor = withStyle(light: .black, dark: .clear), alpha: Float = 0.12, x: CGFloat = 0, y: CGFloat = 2, blur: CGFloat = 4, spread: CGFloat = 0, useShadowPath: Bool = false) {
-		.init(style: UIScreen.main.traitCollection.userInterfaceStyle,
-			  color: color,
-			  alpha: alpha,
-			  x: x,
-			  y: y,
-			  blur: blur,
-			  spread: spread,
-			  useShadowPath: useShadowPath)
+	public init (style: UIUserInterfaceStyle = UIScreen.main.traitCollection.userInterfaceStyle, color: UIColor, alpha: Float = 0.12, x: CGFloat = 0, y: CGFloat = 2, blur: CGFloat = 4, spread: CGFloat = 0, useShadowPath: Bool = false) {
+		self.style = style
+		self.color = color
+		self.alpha = alpha
+		self.x = x
+		self.y = y
+		self.blur = blur
+		self.spread = spread
+		self.useShadowPath = useShadowPath
 	}
 
 	
