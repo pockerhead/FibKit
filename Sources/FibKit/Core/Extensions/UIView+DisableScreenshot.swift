d53c9f7ce673135757fa2a34cac0358cdad2ce59
Artem Balashov <pockerhead@ya.ru>
2024-02-16 12:37:56 +0300

    limit screenshot disable due to crashes on older versions of iOS

--- Sources/FibKit/Core/Extensions/UIView+DisableScreenshot.swift
+++ Sources/FibKit/Core/Extensions/UIView+DisableScreenshot.swift
@@ -10,21 +10,38 @@ import UIKit
 private let textField: UITextField = UITextField()
 private let secureView: UIView? = {
 	textField.subviews.first { view in
-		String(describing: type(of: view)).contains("TextLayoutCanvasView")
+		if #available(iOS 15, *) {
+			return String(describing: type(of: view)).contains("TextLayoutCanvasView")
+		}
+		
+		if #available(iOS 14, *) {
+			return String(describing: type(of: view)).contains("TextFieldCanvasView")
+		}
+		
+		if #available(iOS 13, *) {
+			return String(describing: type(of: view)).contains("TextFieldCanvasView")
+		}
+		
+		if #available(iOS 12, *) {
+			return String(describing: type(of: view)).contains("TextFieldContentView")
+		}
+		return false
 	}
 }()
 
 public func setLayerDisableScreenshots(_ layer: CALayer, _ disableScreenshots: Bool) {
-	guard let secureView = secureView else { return }
-	
-	let previousLayer: CALayer = secureView.layer
-	secureView.setValue(layer, forKey: "layer")
-	if disableScreenshots {
-		textField.isSecureTextEntry = false
-		textField.isSecureTextEntry = true
-	} else {
-		textField.isSecureTextEntry = true
-		textField.isSecureTextEntry = false
+	if #available(iOS 16, *) {
+		guard let secureView = secureView else { return }
+		
+		let previousLayer: CALayer = secureView.layer
+		secureView.setValue(layer, forKey: "layer")
+		if disableScreenshots {
+			textField.isSecureTextEntry = false
+			textField.isSecureTextEntry = true
+		} else {
+			textField.isSecureTextEntry = true
+			textField.isSecureTextEntry = false
+		}
+		secureView.setValue(previousLayer, forKey: "layer")
 	}
-	secureView.setValue(previousLayer, forKey: "layer")
 }
