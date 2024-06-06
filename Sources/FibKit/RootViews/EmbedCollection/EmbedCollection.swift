c0aed5162a22bb9c28d02405c9e376750b057ec3
Denis Sadakov <SadakovDo@it.mos.ru>
2024-06-06 21:35:56 +0300

    fix

--- Sources/FibKit/RootViews/EmbedCollection/EmbedCollection.swift
+++ Sources/FibKit/RootViews/EmbedCollection/EmbedCollection.swift
@@ -376,7 +376,8 @@ extension EmbedCollection: FibViewHeader {
 		formView.layoutSubviews()
 		pageControlBottomConstant = data.pageControlBottomOffset
 		pageControlBottomConstraint?.constant =  pageControlBottomConstant * -1
-		pageControl.numberOfPages = data.provider?.numberOfItems ?? 0//data.sections.first?.dataSource.data.count ?? 0
+		let numberOfPages: Int = (data.provider as? FibKit.FibGridProvider)?.dataSource.data.count ?? 0
+		pageControl.numberOfPages = numberOfPages //data.provider?.numberOfItems ?? 0//data.sections.first?.dataSource.data.count ?? 0
         pagesCount = pageControl.numberOfPages
 		pageControl.isHidden = (!data.pagingEnabled || pageControl.numberOfPages <= 1) || !data.needPageControl
         blurView.isHidden = pageControl.isHidden
