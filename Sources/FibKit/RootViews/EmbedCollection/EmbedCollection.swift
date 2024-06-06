f8f4ff21e57e49821ca67284e9a9d52c57054f3f
Denis Sadakov <SadakovDo@it.mos.ru>
2024-06-06 21:47:01 +0300

    fix

--- Sources/FibKit/RootViews/EmbedCollection/EmbedCollection.swift
+++ Sources/FibKit/RootViews/EmbedCollection/EmbedCollection.swift
@@ -377,6 +377,15 @@ extension EmbedCollection: FibViewHeader {
 		pageControlBottomConstant = data.pageControlBottomOffset
 		pageControlBottomConstraint?.constant =  pageControlBottomConstant * -1
 		let numberOfPages: Int = (data.provider as? FibKit.FibGridProvider)?.dataSource.data.count ?? 0
+		var provider = data.provider as? FibKit.FibGridProvider
+		if let provider = provider {
+			pageControl.numberOfPages = provider.dataSource.data.count
+		} else {
+			provider = ((data.provider as? FibKit.FibGridHeaderProvider)?.sections.first as? FibKit.FibGridProvider)
+			if let provider = provider {
+				pageControl.numberOfPages = provider.dataSource.data.count
+			}
+		}
 		pageControl.numberOfPages = numberOfPages //data.provider?.numberOfItems ?? 0//data.sections.first?.dataSource.data.count ?? 0
         pagesCount = pageControl.numberOfPages
 		pageControl.isHidden = (!data.pagingEnabled || pageControl.numberOfPages <= 1) || !data.needPageControl
