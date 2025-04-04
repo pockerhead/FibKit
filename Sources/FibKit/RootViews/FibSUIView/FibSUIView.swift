8e013494be48e9d5a4154b254bf2fb5a75318828
Artem Balashov <pockerhead@ya.ru>
2025-04-05 01:12:17 +0300

    no message

--- Sources/FibKit/RootViews/FibSUIView/FibSUIView.swift
+++ Sources/FibKit/RootViews/FibSUIView/FibSUIView.swift
@@ -4,29 +4,38 @@ import SwiftUI
 @available(iOS 16.0, *)
 public final class FibSUIView<Content: View>: FibCoreView {
 	
-	private let internalCollectionViewCell = UICollectionViewCell()
+	private var suiContentView: (UIView & UIContentView)!
 	
 	public override func configureUI() {
 		super.configureUI()
-		contentView.addSubview(internalCollectionViewCell)
 	}
 	
 	public override func layoutSubviews() {
 		super.layoutSubviews()
-		internalCollectionViewCell.frame = contentView.frame
+		suiContentView.frame = contentView.frame
 	}
 	
 	public override func sizeWith(_ targetSize: CGSize, data: (any ViewModelWithViewClass)?, horizontal: UILayoutPriority, vertical: UILayoutPriority) -> CGSize? {
 		guard let data = data as? FibSUIViewModel<Content> else { return .zero }
 		configure(with: data)
-		return internalCollectionViewCell.systemLayoutSizeFitting(targetSize, withHorizontalFittingPriority: horizontal, verticalFittingPriority: vertical)
+		return suiContentView.systemLayoutSizeFitting(
+			targetSize,
+			withHorizontalFittingPriority: horizontal,
+			verticalFittingPriority: vertical
+		)
 	}
 	
 	public override func configure(with data: (any ViewModelWithViewClass)?) {
 		super.configure(with: data)
 		guard let data = data as? FibSUIViewModel<Content> else { return }
-		internalCollectionViewCell.contentConfiguration = UIHostingConfiguration(content: { data.content })
+		let config = UIHostingConfiguration(content: { data.content })
 			.margins(.all, 0)
+		if let view = suiContentView {
+			view.configuration = config
+		} else {
+			suiContentView = config.makeContentView()
+			contentView.addSubview(suiContentView)
+		}
 	}
 }
 
